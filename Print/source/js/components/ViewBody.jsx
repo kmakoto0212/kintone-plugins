305dae220c784c19d9444e6463074e7e96ae08e5
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-10-06 16:01:40 +0900

    Titleコンポーネントを別ファイルから読み込むように

--- Print/source/js/components/ViewBody.jsx
+++ Print/source/js/components/ViewBody.jsx
@@ -1,19 +1,14 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Title from './Title.jsx';
 import {RecordTable} from './RecordTable.jsx';
-import {Label} from './Label.jsx';
 import '../../css/header.css';
 
 const ViewBody = ({views, records, fields, title, lines}) => {
 
   return (
     <div className="view-print-body">
-      <Label
-        className="header-title"
-        text={title}
-        fontSize="16px"
-        isVisible={!!title}
-      />
+      <Title title={title} />
       <RecordTable
         className="record-table"
         views={views}
