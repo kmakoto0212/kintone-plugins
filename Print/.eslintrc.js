35b9d8dbaa20f830512c7f6c1aa2d380d70d7d78
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-10-19 11:47:01 +0900

    prettierとの競合を解消

--- Print/.eslintrc.js
+++ Print/.eslintrc.js
@@ -2,23 +2,20 @@ module.exports = {
   env: {
     es2017: true,
   },
-  "globals":{
-    "kintone": "readonly"
+  globals: {
+    kintone: 'readonly',
   },
-  "plugins": [
-    "react",
-    "prettier",
-    "react-hooks"
-  ],
-  "rules": {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "linebreak-style": 0,
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'linebreak-style': 0,
   },
   extends: [
-    "@cybozu/eslint-config/lib/kintone.js",
-    "@cybozu/eslint-config/globals/kintone.js",
-    "@cybozu/eslint-config/presets/react",
-    "plugin:react-hooks/recommended",
+    '@cybozu/eslint-config/lib/kintone.js',
+    '@cybozu/eslint-config/globals/kintone.js',
+    '@cybozu/eslint-config/presets/react',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
 };
