429ed92fd85c6bfa56863aabf059fbf73c85b6f6
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-10-20 18:05:49 +0900

    重複部分をまとめて簡潔に

--- Print/webpack.config.js
+++ Print/webpack.config.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-undef */
 const path = require('path');
-const config = {
-  entry: path.resolve('Print/source/index.jsx'),
+const plugin = {
+  entry: {
+    'Print' : path.resolve('Print/source/index.jsx'),
+    'config' : path.resolve('Print/source/js/config.jsx'),
+  },
   resolve: {
-    extensions: ['.ts', '.jsx', '.js', '.css'],
+    extensions: ['.jsx', '.js', '.css'],
     alias: {
       '@js': path.resolve(__dirname, 'source/js/'),
       '@css': path.resolve(__dirname, 'source/css/'),
@@ -12,7 +15,7 @@ const config = {
   },
   output: {
     path: path.resolve('Print/dist'),
-    filename: 'Print.min.js',
+    filename: '[name].min.js',
   },
   module: {
     rules: [
@@ -45,58 +48,12 @@ const config = {
   },
 };
 
-const configPlugin = {
-  entry: path.resolve('Print/source/js/config.jsx'),
-  resolve: {
-    extensions: ['.ts', '.jsx', '.js'],
-    alias: {
-      '@js': path.resolve(__dirname, 'source/js/'),
-      '@css': path.resolve(__dirname, 'source/css/'),
-      '@components': path.resolve(__dirname, 'source/js/components'),
-    },
-  },
-  output: {
-    path: path.resolve('Print/dist'),
-    filename: 'config.min.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/react'],
-          },
-        },
-      },
-      {
-        test: /.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  performance: {
-    maxEntrypointSize: 10000000,
-    maxAssetSize: 10000000,
-  },
-};
-
 module.exports = (env, argv) => {
   if (argv.mode === 'development') {
-    config.devtool = 'source-map';
-    configPlugin.devtool = 'source-map';
+    plugin.devtool = 'source-map';
   }
   if (argv.mode === 'production') {
     // .....
   }
-  return [config, configPlugin];
+  return plugin;
 };
